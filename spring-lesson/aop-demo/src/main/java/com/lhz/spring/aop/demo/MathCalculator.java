1d6317f86e82f4ce72db6a0710e183e1f47f494a
lhz <2190732276@qq.com>
2020-07-20 18:03:53 +0800

    aop 实例

--- spring-lesson/aop-demo/src/main/java/com/lhz/spring/aop/demo/MathCalculator.java
+++ spring-lesson/aop-demo/src/main/java/com/lhz/spring/aop/demo/MathCalculator.java
@@ -18,7 +18,8 @@ public class MathCalculator {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext beanFactory = new AnnotationConfigApplicationContext();
         beanFactory.register(LogAspects.class,MainConfigOfAOP.class);
-
+        //refresh方法里面的registerBeanPostProcessors(beanFactory);获取带代理
+        //finishBeanFactoryInitialization(beanFactory);获得自己定义的bean，并且产生代理对象
         beanFactory.refresh();
         MathCalculator bean = beanFactory.getBean(MathCalculator.class);
         bean.div(2, 1);
